5644d1a026d20e0fbcbd088a5aecc3377d4db314
Mustafa Zaban <midnight90@mail.bg>
2015-03-12 11:48:07 +0200

    Update Car.h

--- Cars/Car.h
+++ Cars/Car.h
@@ -3,14 +3,13 @@
 
 using namespace std;
 
-class Car
-{
-	friend ostream &operator<< ( ostream &, Car & ); //cout implementation for custom object class "Car"
-	friend istream &operator>> ( istream &, Car & ); //cin implementation for custom object class "Car"
+class Car {
+
+	friend ostream & operator<< ( ostream &, Car & ); //cout implementation for custom object class "Car"
+	friend istream & operator>> ( istream &, Car & ); //cin implementation for custom object class "Car"
 
 private:
-	
-	// class members
+	// custom object members
 	string m_sBrand;
 	string m_sModel;
 	int m_nYear;
@@ -18,11 +17,10 @@ private:
 	string m_sColor;
 
 public:
-	
-	Car (); //default constructor
-	Car ( string, string, int, double, string ); //custom constructor with full range of private members
-	Car ( const Car & ); //copy constructor
-	
+	Car( ); //default constructor
+	Car( string, string, int, double, string ); //custom constructor with full range of private members
+	Car( const Car & ); //copy constructor
+
 	//setters for all private members
 	void setBrand ( string );
 	void setModel ( string );
@@ -30,23 +28,19 @@ public:
 	void setConsumption ( double );
 	void setColor ( string );
 
-        //getters for all private members  
-	string getBrand ();
-	string getModel ();
-	int getYear ();
-	double getConsumption ();
-	string getColor ();
+	//getters for all private members
+	string getBrand ( );
+	string getModel ( );
+	int getYear ( );
+	double getConsumption ( );
+	string getColor ( );
 
-        //predefined operator = for custom class "Car"   
+	//predefined operator = for custom class "Car"
 	Car operator= ( const Car & );
-	
-	//predefined operator == for custom class "Car"   
+
+	//predefined operator == for custom class "Car"
 	bool operator== ( const Car & ) const;
-	
-	//predefined operator != for custom class "Car"   
+
+	//predefined operator != for custom class "Car"
 	bool operator!= ( const Car & ) const;
-
-
-
 };
-
