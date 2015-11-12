d89c2f1c0b7e372002a98f209e8da60700acc7d5
Mustafa Zaban <tayrun@abv.bg>
2015-11-12 21:45:14 +0200

    Container
    custom class implements container for integer type numbers

---
+++ Container/Container.cpp
@@ -0,0 +1,238 @@
+#include "Container.h"
+
+Container::Container()
+{
+	c_First = c_Last = NULL;
+	c_size = 0;
+}
+
+Container::~Container()
+{
+	this->clear();
+
+}
+
+Container::Container(Container & rhs)
+{
+	for (int i = 0; i < rhs.getSize(); ++i)
+	{
+		this->putElementAtEnd(rhs.getElementsValueAt(i));
+	}
+}
+
+void Container::putElementAtBegin(int valueToInsert)
+{
+	Element * toInsert = new Element;
+	toInsert->e_value = valueToInsert;
+
+	if (this->isEmpty())
+	{
+		toInsert->e_next = NULL;
+		toInsert->e_previus = NULL;
+		this->c_First = this->c_Last = toInsert;
+		toInsert->e_number = c_size;
+		c_size++;
+	}
+	else
+	{
+		Element * toCheck = new Element;
+		toCheck = this->c_First;
+
+		while (toCheck->e_next != NULL)
+		{
+			toCheck->e_number++;
+			toCheck = toCheck->e_next;
+		}
+
+		this->c_First->e_previus = toInsert;
+		toInsert->e_next = this->c_First;
+		toInsert->e_previus = NULL;
+		this->c_First = toInsert;
+		toInsert->e_number = 0;
+		c_size++;
+
+
+	}
+}
+
+void Container::putElementAtEnd(int valueToInsert)
+{
+	Element * toInsert = new Element;
+	toInsert->e_value = valueToInsert;
+
+	if (this->isEmpty())
+	{
+		toInsert->e_next = NULL;
+		toInsert->e_previus = NULL;
+		this->c_First = this->c_Last = toInsert;
+		toInsert->e_number = c_size;
+		c_size++;
+	}
+	else
+	{
+		this->c_Last->e_next = toInsert;
+		toInsert->e_previus = this->c_Last;
+		toInsert->e_next = NULL;
+		this->c_Last = toInsert;
+		toInsert->e_number = c_size + 1;
+		c_size++;
+	}
+}
+int Container::getFirstElement()
+{
+	int valueToReturn = 0;
+
+	if (this->isEmpty())
+	{
+		return 0;
+	}
+	else if (this->getSize() == 1)
+	{
+		valueToReturn = this->c_First->e_value;
+		delete this->c_First;
+		c_size--;
+		return valueToReturn;
+	}
+	else
+	{
+		valueToReturn = this->c_First->e_value;
+		this->c_First = this->c_First->e_next;
+		delete this->c_First->e_previus;
+		this->c_First->e_previus = NULL;
+		c_size--;
+		return valueToReturn;
+	}
+
+}
+
+int Container::getLastElement()
+{
+	int valueToReturn = 0;
+
+	if (this->isEmpty())
+	{
+		return 0;
+	}
+	else if (this->getSize() == 1)
+	{
+		valueToReturn = this->c_Last->e_value;
+		delete this->c_Last;
+		c_size--;
+		return valueToReturn;
+	}
+	else
+	{
+		valueToReturn = this->c_Last->e_value;
+		this->c_Last = this->c_Last->e_previus;
+		delete this->c_Last->e_next;
+		this->c_Last->e_next = NULL;
+		c_size--;
+		return valueToReturn;
+	}
+}
+
+void Container::putElementAtPostition(int valueToInsert, int position)
+{
+	if (this->isEmpty() || (position == 0) || (position == 1))
+	{
+		putElementAtBegin(valueToInsert);
+	}
+	else if (position > this->getSize())
+	{
+		putElementAtEnd(valueToInsert);
+	}
+	else
+	{
+		Element * toCheck = new Element;
+		toCheck = this->c_First;
+		for (int i = 0; i < this->getSize(); ++i)
+		{
+			if ((int)toCheck->e_number != (int)position)
+			{
+				toCheck = toCheck->e_next;
+			}
+
+		}
+
+
+		Element * toInsert = new Element;
+		toCheck->e_previus->e_next = toInsert;
+		toInsert->e_previus = toCheck->e_previus;
+		toCheck->e_previus = toInsert;
+		toInsert->e_next = toCheck;
+		toInsert->e_value = valueToInsert;
+		toInsert->e_number = toCheck->e_number - 1;
+		c_size++;
+
+
+		while (toCheck->e_next != NULL)
+		{
+			toCheck->e_number++;
+			toCheck = toCheck->e_next;
+		}
+
+	}
+}
+
+
+int Container::getElementsValueAt(int position)
+{
+	if (position == 0)
+	{
+		return this->c_First->e_value;
+		this->c_First = this->c_First->e_next;
+
+	}
+	else if (position == this->getSize())
+	{
+		return this->c_Last->e_value;
+	}
+	else {
+
+
+		Element * checker = this->c_First;
+		for (int i = 0; i < position; ++i)
+		{
+			checker = checker->e_next;
+		}
+
+		return checker->e_value;
+	}
+
+}
+
+
+Container & Container::operator=(Container & rhs)
+{
+	for (int i = 0; i < rhs.getSize(); ++i)
+	{
+		this->putElementAtEnd(rhs.getElementsValueAt(i));
+	}
+
+	return  *this;
+}
+
+bool Container::isEmpty()
+{
+	if (this->getSize() == 0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+int Container::getSize()
+{
+	return c_size;
+}
+
+void Container::clear()
+{
+	while (this->isEmpty() == false)
+	{
+		this->getLastElement();
+	}
+}
---
+++ Container/Container.h
@@ -0,0 +1,61 @@
+/*
+* Container.h
+*
+*  Created on: Nov 2, 2015
+*      Author: Mustafa Zaban
+*      IDE: Eclipse
+*/
+#include <iostream>  //fundamental input/output library
+
+using namespace std;
+
+#ifndef CONTAINER_H_
+#define CONTAINER_H_
+
+
+class Container {
+
+public:
+	Container();  //constructor
+	virtual ~Container();  //destructor
+	Container & operator=(Container &); //operator=
+	Container(Container &);  //copy constructor
+
+
+							 //element adding functions
+	void putElementAtBegin(int);
+	void putElementAtEnd(int);
+	//                       value,position
+	void putElementAtPostition(int, int);
+
+
+	//element getting functions
+	int getFirstElement();  //pop the first element
+	int getLastElement();   //pop the last element
+	int getElementsValueAt(int);  //get the Nth elements value
+
+								  //other functions
+	bool isEmpty();  //check if the container is empty
+	int getSize();   //gets the containers size
+	void clear();    //clear the container
+
+
+private:
+
+	int c_size;  //containers size
+
+	struct Element  //elements structure (the node from container chain)
+	{
+		int e_value;
+		Element * e_next;
+		Element * e_previus;
+		unsigned int e_number;
+	};
+
+	Element * c_First; //points to the first element
+	Element * c_Last;  //points to the last element
+
+
+};
+
+#endif /* CONTAINER_H_ */
